bdbae2106a58c78d03fc565b7723b72d59219df1
zyfine <zyfine@foxmail.com>
2019-11-21 23:22:12 +0800

    补充ArrayList包

--- src/main/java/com/hm/hm_page/controller/UserController.java
+++ src/main/java/com/hm/hm_page/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
